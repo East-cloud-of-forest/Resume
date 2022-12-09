8c10ff4b562a122922adfa87d9ff14d266ef5e26
dongun lim <ecof1911@gmail.com>
2022-12-09 16:09:16 +0900

    skill update, footer 디자인 변경

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.scss";
-import Header from "./components/Header";
-import Resume from "./Pages/Resume";
-import project from "./data/project";
-import classNames from "classnames";
-import Project from "./Pages/Project";
-import { useState } from "react";
-import Error from "./Pages/Error";
+import { Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Header from './components/Header'
+import Resume from './Pages/Resume'
+import project from './data/project'
+import classNames from 'classnames'
+import Project from './Pages/Project'
+import { useState } from 'react'
+import Error from './Pages/Error'
 
 function App() {
   const [dark, setDark] = useState(true)
 
   return (
-    <div className={classNames("App", dark||'white')}>
+    <div className={classNames('App', dark || 'white')}>
       <div className="main">
         <Header setDark={setDark} dark={dark} />
         <Routes>
@@ -20,12 +20,12 @@ function App() {
           <Route path="/project" element={<Project project={project} />} />
           <Route path="/*" element={<Error />} />
         </Routes>
-        <div className={classNames("footer", "section")}>
-          Ecof<span>&copy;</span>
-        </div>
+      </div>
+      <div className={classNames('footer', 'section')}>
+        Ecof<span>&copy;</span>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/data/skill.js
+++ src/data/skill.js
@@ -10,9 +10,23 @@ const skill = [
     Canvas를 활용한 Interactive에 관심이 있고 프로젝트에 활용할 방법을 공부하고 있습니다.`
   },
   {
+    name : 'Typescript',
+    text : `Javascript의 type을 설정하여 에러와 버그를 사전 차단하고 생산성을 높일 수 있습니다. 함수 및 변수의 타입과 배열, 객체, 클래스 등의 타입과 형태를 설정할 수 있습니다. <br/>
+    d.ts 파일을 작성하거나 npm에 설정된 파일을 가져와 Javascript 파일 및 npm과 원활히 호환할 수 있습니다.`
+  },
+  {
     name : 'React.js',
     text : `함수형, 클래스형 Components, Hooks, 라이프 사이클 등을 이용하여 SPA를 작성할 수 있습니다. <br/>
     재사용 로직을 위해 커스텀 Hooks과 Suspense, React Query 등 React의 최신 기술 등을 공부하고 있습니다.`
+  },
+  {
+    name: 'Next.js',
+    text: `React를 활용한 SEO에 최적화된 SSR을 페이지 단위로 설계하여 웹페이지를 작성할 수 있습니다. <br/>`
+  },
+  {
+    name : 'Vue.js',
+    text : `라이프 사이클을 적절히 활용하고 vuex를 이용한 전역 상태관리를 이용해 SPA를 작성할 수 있습니다. <br/>
+    vuetify를 이용해 빠르고 효율적이게 웹페이지에 디자인을 적용할 수 있습니다.`
   },
   {
     name : 'Redux',
@@ -21,22 +35,25 @@ const skill = [
     API의 비동기 통신 및 여러 기능을 위한 middleware를 공부하고 있습니다.`
   },
   {
-    name : 'Vue.js',
-    text : `라이프 사이클을 적절히 활용하고 vuex를 이용한 전역 상태관리를 이용해 SPA를 작성할 수 있습니다. <br/>
-    vuetify를 이용해 빠르고 쉽게 웹페이지에 디자인을 적용할 수 있습니다.`
-  },
-  {
     name : 'Back End',
     text : `Firebase - Storage, auth, Database 등을 사용할 수 있고 다른 다양한 기능을 공부하고 있습니다. <br/>
     AWS - Lambda를 활용해 API를 배포할 수 있습니다. <br/>
     node.js - express를 활용해 간단한 서버를 만들고 프론트엔드와 연결 할 수 있습니다.`
   },
   {
+    name : "Style",
+    text:`SASS - Css 보다 높은 가독성의 스타일을 작성할 수 있습니다. 함수 및 연산자 등의 기능을 사용할 수 있습니다. <br/>
+    Styled Componenets - 컴포넌트별 스타일 관리 및 Js 문법을 활용한 Css 작성이 가능합니다. <br/>
+    Bootstrap - 템플릿 및 테마를 활용하여 빠르고 효율적으로 스타일을
+    구성할 수 있습니다. <br/>
+    Tailwind Css - 템플릿과 클래스를 활용한 스타일과 테마를 설정하고
+    쉽게 반응형으로 설계할 수 있으며 재사용 가능하도록 추상화 할 수 있습니다.
+    `
+  },
+  {
     name : 'etc',
     text : `Git - 프로젝트의 버전관리를 할 수 있습니다. 커밋을 깔끔하게 작성할 수 있도록 공부하고 있습니다. <br/>
-    SASS - Css 보다 높은 가독성의 스타일을 작성할 수 있습니다. 함수 및 연산자 등의 기능을 사용할 수 있습니다. <br/>
-    jQuery - 문법을 이용해 동적 웹페이지를 작성할 수 있습니다. <br/>
-    Styled Componenets - 컴포넌트별 스타일 관리 및 Js 문법을 활용한 Css 작성이 가능합니다.`
+    jQuery - 문법을 이용해 동적 웹페이지를 작성할 수 있습니다. <br/>`
   },
   {
     name : 'Design',
